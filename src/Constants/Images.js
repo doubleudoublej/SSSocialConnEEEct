d5fba0f1d5316da5a33ba29add61e7eeccb8f527
doubleudoublej <jiajun2910@gmail.com>
2025-02-06 09:06:19 +0800

    update to deployment

--- src/Constants/Images.js
+++ src/Constants/Images.js
@@ -4,9 +4,6 @@ import close from "../assets/close.svg";
 import logo from "../assets/logo.png";
 import menu from "../assets/menu.svg";
 import arrowup from "../assets/arrowup.svg";
-import star from "../assets/star.svg";
-import shield from "../assets/shield.svg";
-import send from "../assets/send.svg";
 import people1 from "../assets/people01.png";
 import people2 from "../assets/people02.png";
 import people3 from "../assets/people03.png";
@@ -18,9 +15,6 @@ export {
   logo,
   menu,
   arrowup,
-  star,
-  shield,
-  send,
   people1,
   people2,
   people3,
